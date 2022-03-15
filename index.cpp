d7e4bdc1360ca361da1b93108f8bd8374d9446b2
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-14 20:13:16 -0500

    added 8 to the switch table

--- index.cpp
+++ index.cpp
@@ -7,15 +7,15 @@ struct state {
     int result;
     int turn = 0;
     state *attacks[4];
-    state *swiches[2];
+    state *swiches[3];
 };
 
-//                          0, 1, 2, 3, 4, 5, 6, 7, 8
-const int statecounts[] = {0, 1, 2, 2, 3, 2, 2, 1};
+//                         0, 1, 2, 3, 4, 5, 6, 7, 8
+const int statecounts[] = {0, 1, 2, 2, 3, 2, 2, 1, 1};
 const int switchtable[] = {
-    0, 1, 2, 3, 4, 4, 4, 4,
-    0, 1, 1, 2, 3, 3, 3, 4,
-    0, 1, 1, 2, 2, 3, 3, 4,
+    0, 1, 2, 3, 4, 4, 4, 4, 4,
+    0, 1, 1, 2, 3, 3, 3, 4, 4,
+    0, 1, 1, 2, 2, 3, 3, 4, 4,
 };
 
 state *getmoves(int ph1, int ph2, int oh1, int oh2)
